3fc614480b8da29aad090c81451847f5ee3dd325
Shoki11 <20cm0119@jec.ac.jp>
2021-06-23 09:57:20 +0900

    最新

--- SampleToDo/LoadData.swift
+++ SampleToDo/LoadData.swift
@@ -24,25 +24,24 @@ class LoadData {
             } else {
                 
                 var ToDoArray: [ToDoData] = []
-                
-                for document in snapshot!.documents {
-                    
-                    print("aaaaaaaaaa\(document.data())")
-                    
-                    let data = document.data()
-
-                    if let userID = data["userID"] as? String,
-                       let userName = data["userName"] as? String,
-                        let text = data["text"] as? String,
-                        let date = data["date"] as? Double {
-
-                        let newToDoData = ToDoData(userName: userName, userID: userID, text: text, date: date)
+                snapshot?.documentChanges.forEach({ (documentChanges) in
+                    switch documentChanges.type {
+                    case .added:
+                        let data = documentChanges.document.data()
                         
-                        ToDoArray.append(newToDoData)
-                        
+                        if let userID = data["userID"] as? String,
+                           let userName = data["userName"] as? String,
+                           let text = data["text"] as? String,
+                           let date = data["date"] as? Double {
+                            
+                            let newToDoData = ToDoData(userName: userName, userID: userID, text: text, date: date)
+                            
+                            ToDoArray.append(newToDoData)
+                        }
+                    case .modified, .removed:
+                        print("消されたときの処理")
                     }
-
-                }
+                })
                 
                 completion(.success(ToDoArray))
                 
